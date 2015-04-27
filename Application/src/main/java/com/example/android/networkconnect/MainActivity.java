e15a236993de66b1e70d00d284b8d75af2f30b11
lijiancheng0614 <lijiancheng0614@gmail.com>
2015-04-27 12:25:02 +0800

    Separate DownloadTask from MainActivity.

--- Application/src/main/java/com/example/android/networkconnect/MainActivity.java
+++ Application/src/main/java/com/example/android/networkconnect/MainActivity.java
@@ -16,7 +16,6 @@
 
 package com.example.android.networkconnect;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.TypedValue;
@@ -29,19 +28,11 @@ import com.example.android.common.logger.LogFragment;
 import com.example.android.common.logger.LogWrapper;
 import com.example.android.common.logger.MessageOnlyLogFilter;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 /**
  * Sample application demonstrating how to connect to the network and fetch raw
  * HTML. It uses AsyncTask to do the fetch on a background thread. To establish
  * the network connection, it uses HttpURLConnection.
- *
+ * <p/>
  * This sample uses the logging framework to display log output in the log
  * fragment (LogFragment).
  */
@@ -60,7 +51,7 @@ public class MainActivity extends FragmentActivity {
 
         // Initialize text fragment that displays intro text.
         SimpleTextFragment introFragment = (SimpleTextFragment)
-                    getSupportFragmentManager().findFragmentById(R.id.intro_fragment);
+                getSupportFragmentManager().findFragmentById(R.id.intro_fragment);
         introFragment.setText(R.string.welcome_message);
         introFragment.getTextView().setTextSize(TypedValue.COMPLEX_UNIT_DIP, 16.0f);
 
@@ -80,96 +71,20 @@ public class MainActivity extends FragmentActivity {
             // When the user clicks FETCH, fetch the first 500 characters of
             // raw HTML from editText
             case R.id.fetch_action:
-                EditText editText = (EditText)findViewById(R.id.textView);
+                EditText editText = (EditText) findViewById(R.id.textView);
                 new DownloadTask().execute(editText.getText().toString());
                 return true;
             // Clear the log view fragment.
             case R.id.clear_action:
-              mLogFragment.getLogView().setText("");
-              return true;
+                mLogFragment.getLogView().setText("");
+                return true;
         }
         return false;
     }
 
     /**
-     * Implementation of AsyncTask, to fetch the data in the background away from
-     * the UI thread.
+     * Create a chain of targets that will receive log data
      */
-    private class DownloadTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... urls) {
-            try {
-                return loadFromNetwork(urls[0]);
-            } catch (IOException e) {
-              return getString(R.string.connection_error);
-            }
-        }
-
-        /**
-         * Uses the logging framework to display the output of the fetch
-         * operation in the log fragment.
-         */
-        @Override
-        protected void onPostExecute(String result) {
-          Log.i(TAG, result);
-        }
-    }
-
-    /** Initiates the fetch operation. */
-    private String loadFromNetwork(String urlString) throws IOException {
-        InputStream stream = null;
-        String str ="";
-
-        try {
-            stream = downloadUrl(urlString);
-            str = readIt(stream, 500);
-       } finally {
-           if (stream != null) {
-               stream.close();
-            }
-        }
-        return str;
-    }
-
-    /**
-     * Given a string representation of a URL, sets up a connection and gets
-     * an input stream.
-     * @param urlString A string representation of a URL.
-     * @return An InputStream retrieved from a successful HttpURLConnection.
-     * @throws java.io.IOException
-     */
-    private InputStream downloadUrl(String urlString) throws IOException {
-        // BEGIN_INCLUDE(get_inputstream)
-        URL url = new URL(urlString);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setReadTimeout(10000 /* milliseconds */);
-        conn.setConnectTimeout(15000 /* milliseconds */);
-        conn.setRequestMethod("GET");
-        conn.setDoInput(true);
-        // Start the query
-        conn.connect();
-        InputStream stream = conn.getInputStream();
-        return stream;
-        // END_INCLUDE(get_inputstream)
-    }
-
-    /** Reads an InputStream and converts it to a String.
-     * @param stream InputStream containing HTML from targeted site.
-     * @param len Length of string that this method returns.
-     * @return String concatenated according to len parameter.
-     * @throws java.io.IOException
-     * @throws java.io.UnsupportedEncodingException
-     */
-    private String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
-        Reader reader = null;
-        reader = new InputStreamReader(stream, "UTF-8");
-        char[] buffer = new char[len];
-        reader.read(buffer);
-        return new String(buffer);
-    }
-
-    /** Create a chain of targets that will receive log data */
     public void initializeLogging() {
 
         // Using Log, front-end to the logging chain, emulates
